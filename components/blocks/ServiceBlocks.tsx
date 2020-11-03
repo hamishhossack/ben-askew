da6a5071ab8d5fa41476a46f70999b1f5b95c4cd
Hamish Hossack <hamishhossack@gmail.com>
2020-11-03 21:17:28 +0000

    fix dynamic class purging

--- components/blocks/ServiceBlocks.tsx
+++ components/blocks/ServiceBlocks.tsx
@@ -8,6 +8,7 @@ export type ServiceContent = {
   description: string
   color: string
   animation: string
+  index?: number
 }
 
 type Props = {
@@ -21,29 +22,33 @@ type Props = {
   content: string
 }
 
+function Block({ index = 0, title, animation, color, description, icon }: ServiceContent) {
+  const padding = index % 2 === 0 ? `lg:pt-${index === 0 ? '12' : '6'}` : ''
+  const colorClass = `bg-${color}-400`
+  return (
+    <div key={title} data-aos={animation} className={`${padding} w-full md:w-4/12 px-4 text-center`}>
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+        <div className="px-4 py-5 flex-auto">
+          <div
+            className={`text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full ${colorClass}`}
+          >
+            <FontAwesomeIcon icon={['fas', icon as IconName]} />
+          </div>
+          <h6 className="text-xl font-semibold">{title}</h6>
+          <p className="mt-2 mb-4 text-gray-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 function ServiceBlocks({ services, icon, featured, content }: Props) {
   return (
     <section className="pb-20 bg-gray-300 -mt-24">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap">
-          {services.map(({ icon, title, description, color, animation }, i) => (
-            <div
-              key={title}
-              data-aos={animation}
-              className={`${i % 2 === 0 ? `lg:pt-${i === 0 ? '12' : '6'}` : ''} w-full md:w-4/12 px-4 text-center`}
-            >
-              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                <div className="px-4 py-5 flex-auto">
-                  <div
-                    className={`text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-${color}-400`}
-                  >
-                    <FontAwesomeIcon icon={['fas', icon as IconName]} />
-                  </div>
-                  <h6 className="text-xl font-semibold">{title}</h6>
-                  <p className="mt-2 mb-4 text-gray-600">{description}</p>
-                </div>
-              </div>
-            </div>
+          {services.map((service, i) => (
+            <Block index={i} {...service} />
           ))}
         </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      whitelist: ['bg-red-400', 'bg-green-400', 'bg-blue-400', 'lg:pt-12', 'lg:pt-6'],
+    },
+  },
   theme: {
     extend: {
       colors: {
