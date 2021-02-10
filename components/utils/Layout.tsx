d630cfe9793316add0a3bc64039097fdbee6a83e
Hamish Hossack <hamishhossack@gmail.com>
2021-02-10 13:47:36 +0000

    fix logo images

--- components/utils/Layout.tsx
+++ components/utils/Layout.tsx
@@ -19,7 +19,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       </title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" type="image/png" href="/logo.png" />
+      <link rel="icon" type="image/png" href="/logo.webp" />
       <meta property="og:title" content={`${settings.site_title} | ${title}`} />
       <meta property="og:description" content={settings.site_description} />
       <meta property="og:image" content="/logo-with-bg.png" />
