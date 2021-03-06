37a270f07ea245a125245a7c81b0973f566e7711
Hamish Hossack <hamishhossack@gmail.com>
2021-03-06 16:43:47 +0000

    update mailgun lib

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,12 +1,14 @@
-const mailgun = require('mailgun-js')
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
 const domain = 'mg.bwaelectrical.com'
-const apiKey = process.env.MAILGUN_API_KEY
-const mg = mailgun({ apiKey, domain })
 const mgDefaults = {
   from: 'Website Contact <postmaster@bwaelectrical.com>',
   to: 'info@bwaelectrical.com',
   subject: 'Website contact',
 }
+
+const mailgun = new Mailgun(formData)
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY, url: 'https://api.eu.mailgun.net' })
 
 exports.handler = async (event, context) => {
   // Only allow POST
@@ -22,7 +24,12 @@ exports.handler = async (event, context) => {
 
       ${data.body}
     `
-    await mg.messages().send({ ...mgDefaults, text })
+    await mg.messages.create(domain, {
+      from: 'Web Admin <postmaster@bwaelectrical.com>',
+      to: ['info@bwaelectrical.com'],
+      subject: 'Website Contact',
+      text,
+    })
   } catch (e) {
     console.error(e)
     return {
